008d4d3e7f0a26ad2df559fd0101353fc448ddb8
AbhilashMadi <madiabhilash1563@gmail.com>
2023-10-15 02:57:51 +0530

    intrensic observer

--- src/components/containers/features/DashboardFeatures.tsx
+++ src/components/containers/features/DashboardFeatures.tsx
@@ -8,7 +8,7 @@ import { featuresConfig } from "@temp/featuresConfig";
 const DashboardFeatures: FC = () => {
   return (
     <section className="p-8 space-y-8">
-      <div className="flex items-center justify-between flex-wrap">
+      <div className="flex items-center justify-between flex-wrap gap-4">
         <h2 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl bg-gradient-to-br from-zinc-900 to-neutral-50 dark:to-zinc-900 dark:from-neutral-50 inline-block text-transparent bg-clip-text animate-fade-up animate-delay-100">{featuresConfig.headline}</h2>
         <Link to="#" className="flex gap-3 font-medium underline underline-offset-4 text-primary">{featuresConfig.link}<ChevronRight className="animate-fade-right"/></Link>
       </div>
