d88ab35466be7cca9869d1c25877b1222165f3aa
AbhilashMadi <madiabhilash1563@gmail.com>
2023-10-15 04:57:38 +0530

    redirection section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as HeroBackground } from "./herobackgrond.svg";
-export { default as HeroImage} from "./heroImage.svg";
+export { default as HeroImage } from "./heroImage.svg";
+export { default as DashboardIcon } from "./dashbordicon.svg";
+export { default as TryForFreeIcon } from "./tryforfreeicon.svg";
+export { default as AboutIcon } from "./abouticon.svg";
