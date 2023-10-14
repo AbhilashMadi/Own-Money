85e0bc2f02022af5ca517435ba00a66171039b65
AbhilashMadi <madiabhilash1563@gmail.com>
2023-10-14 20:48:18 +0530

    hero section responsive fixes

--- src/components/containers/hero/Hero.tsx
+++ src/components/containers/hero/Hero.tsx
@@ -9,19 +9,19 @@ const Hero: FC = () => {
       <p className="leading-7 [&:not(:first-child)]:mt-6 uppercase font-medium space-x-1 
           tracking-[0.15rem] hover:tracking-[0.25rem] 
           transition-all duration-500 font-mont">{heroConfig.tagline}</p>
-      <div className="grid grid-cols-4">
-        <div className="col-span-3 space-y-5">
+      <div className="flex flex-wrap gap-4 xl:gap-0">
+        <div className="space-y-5 w-full xl:w-3/4">
           <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">{heroConfig.headline}</h1>
           <p className="leading-6 [&:not(:first-child)]:mt-6 font-mont font-medium">{heroConfig.description}</p>
         </div>
-        <div>
-          <img src={heroConfig.heroImage} alt="Hero Image" className="object-cover w-[225px] h-[225px] mx-auto animate-spin animate-infinite animate-duration-[7000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-both" />
-        </div>
+        <figure className="w-full xl:w-1/4">
+          <img src={heroConfig.heroImage} alt="Hero Image" className="object-cover w-auto h-[225px] mx-auto animate-spin animate-infinite animate-duration-[7000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-both" />
+        </figure>
       </div>
 
-      <div className="grid grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
         {heroConfig.highlights.map((item) => {
-          return <FeatureItem item={item} key={item.headline}/>;
+          return <FeatureItem item={item} key={item.headline} />;
         })}
       </div>
     </section >
