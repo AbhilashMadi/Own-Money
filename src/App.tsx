8fd7576cd9f936d4b56302d12012234ae52a51b3
AbhilashMadi <madiabhilash1563@gmail.com>
2023-11-02 00:41:33 +0530

    top progress bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { FC, Suspense } from "react";
 import "@styles/index.css";
+import LoadingBar from "react-top-loading-bar";
 
 import PageRoutes from "@routes/PageRoutes";
 
 const App: FC = () => {
   return (
-    <Suspense fallback={<>Loading...</>}>
+    <Suspense fallback={<LoadingBar
+      progress={100}
+      color="#f17020"
+      shadow />}>
       <PageRoutes />
     </Suspense>
   );
