dac8997b45213510ff613009cf3d56f8058e2f0a
AbhilashMadi <madiabhilash1563@gmail.com>
2023-11-02 02:40:12 +0530

    recent transactions

--- src/components/containers/atoms/RecentTransactions.tsx
+++ src/components/containers/atoms/RecentTransactions.tsx
@@ -1,8 +1,111 @@
 import { Card } from "@ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/tabs";
-import { FC } from "react";
+import { FC, ReactNode } from "react";
+import { Gamepad, ShoppingBag } from "lucide-react";
+
+enum TransactionKeys {
+  REVENUE = "revenue",
+  EXPENSES = "expenses",
+}
+
+type Transaction = {
+  title: string;
+  icon: ReactNode;
+  category: string;
+  price: string;
+  date: string;
+  type: TransactionKeys,
+}
+
+const ExpensesCard: FC<{ obj: Transaction }> = (props) => {
+  const { obj } = props;
+
+  return <div className="flex gap-4 py-2 items-center border-b">
+    <div className="bg-muted p-4 grid items-center rounded-lg">{obj.icon}</div>
+    <div className="grow">
+      <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">{obj.title}</h4>
+      <p className="text-gray-400 text-sm">{obj.category}</p>
+    </div>
+    <div>
+      <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">{obj.price}</h4>
+      <span className="text-gray-400 text-sm">{obj?.date}</span>
+    </div>
+  </div>;
+};
 
 const RecentTransactions: FC = () => {
+  const tempTransactions: Transaction[] = [{
+    icon: <Gamepad />,
+    title: "GTR 5",
+    category: "Gadget & Gear",
+    price: "$160.00",
+    date: "17 May 2023",
+    type: TransactionKeys.REVENUE,
+  }, {
+    icon: <Gamepad />,
+    title: "GTR 5",
+    category: "Gadget & Gear",
+    price: "$160.00",
+    date: "17 May 2023",
+    type: TransactionKeys.REVENUE,
+  }, {
+    icon: <Gamepad />,
+    title: "GTR 5",
+    category: "Gadget & Gear",
+    price: "$160.00",
+    date: "17 May 2023",
+    type: TransactionKeys.REVENUE,
+  }, {
+    icon: <Gamepad />,
+    title: "GTR 5",
+    category: "Gadget & Gear",
+    price: "$160.00",
+    date: "17 May 2023",
+    type: TransactionKeys.REVENUE,
+  }, {
+    icon: <Gamepad />,
+    title: "GTR 5",
+    category: "Gadget & Gear",
+    price: "$160.00",
+    date: "17 May 2023",
+    type: TransactionKeys.REVENUE,
+  }, {
+    icon: <ShoppingBag />,
+    title: "Keyboard",
+    category: "Gadget & Gear",
+    price: "$22.00",
+    date: "17 May 2023",
+    type: TransactionKeys.EXPENSES,
+  }, {
+    icon: <ShoppingBag />,
+    title: "Keyboard",
+    category: "Gadget & Gear",
+    price: "$22.00",
+    date: "17 May 2023",
+    type: TransactionKeys.EXPENSES,
+  }, {
+    icon: <ShoppingBag />,
+    title: "Keyboard",
+    category: "Gadget & Gear",
+    price: "$22.00",
+    date: "17 May 2023",
+    type: TransactionKeys.EXPENSES,
+  }, {
+    icon: <ShoppingBag />,
+    title: "Keyboard",
+    category: "Gadget & Gear",
+    price: "$22.00",
+    date: "17 May 2023",
+    type: TransactionKeys.EXPENSES,
+  }, {
+    icon: <ShoppingBag />,
+    title: "Keyboard",
+    category: "Gadget & Gear",
+    price: "$22.00",
+    date: "17 May 2023",
+    type: TransactionKeys.EXPENSES,
+  }];
+
   return (
     <>
       <p className="font-sm text-gray-400 pb-2">Recent Transactions</p>
@@ -14,13 +117,19 @@ const RecentTransactions: FC = () => {
             <TabsTrigger value="expenses">Expenses</TabsTrigger>
           </TabsList>
           <TabsContent value="all">
-            all
+            {tempTransactions.map((obj, index: number) => {
+              return <ExpensesCard key={index} obj={obj} />;
+            })}
           </TabsContent>
           <TabsContent value="revenue">
-            revenue
+            {tempTransactions.filter((obj) => obj.type === TransactionKeys.REVENUE).map((obj, index: number) => {
+              return <ExpensesCard key={index} obj={obj} />;
+            })}
           </TabsContent>
           <TabsContent value="expenses">
-            expenses
+            {tempTransactions.filter((obj) => obj.type === TransactionKeys.EXPENSES).map((obj, index: number) => {
+              return <ExpensesCard key={index} obj={obj} />;
+            })}
           </TabsContent>
         </Tabs>
       </Card>
