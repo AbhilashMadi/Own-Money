8ccd703423c3a310e7a6cd75b018a0d3c742dadf
AbhilashMadi <madiabhilash1563@gmail.com>
2023-11-02 01:50:13 +0530

    sticky and blur navbar

--- src/components/common/DashboardHeader.tsx
+++ src/components/common/DashboardHeader.tsx
@@ -13,7 +13,8 @@ const DashboardHeader: FC = () => {
 
 
   return (
-    <header className="p-4 flex justify-between border border-l-0">
+    <header className="p-3 flex justify-between border-b bg-white dark:bg-black sticky top-0 left-0 
+      right-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 z-30">
       <div className="grow-[1] flex align-middle gap-4">
         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Hello @User</h4>
         <div className="flex text-gray-400 gap-4 items-center">
