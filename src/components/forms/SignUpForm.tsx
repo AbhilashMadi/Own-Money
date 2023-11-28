01e3dce0ad1ad39f6436190e06c2fff9d9db4b97
AbhilashMadi <madiabhilash1563@gmail.com>
2023-11-28 21:57:41 +0530

    signup form

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -1,13 +1,50 @@
-import { FC } from "react";
-import { Input } from "@ui/input";
-import { Button } from "@ui/button";
-import { Label } from "@ui/label";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@ui/card";
 import { headers } from "@resources/headers";
-import { text } from "@resources/text";
 import { labels } from "@resources/labels";
+import { text } from "@resources/text";
+import { Button } from "@ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@ui/card";
+import { Input } from "@ui/input";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui/form";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Email must be a valid email" }),
+  password: z.string().min(8, { message: "Password should be at least 8 characters" }),
+  confirmPassword: z.string().min(8, { message: "Password should be at least 8 characters" }),
+}).refine(
+  ({ password, confirmPassword }) => password === confirmPassword,
+  {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  },
+);
 
 const SignUpForm: FC = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -17,22 +54,58 @@ const SignUpForm: FC = () => {
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-2">
-        <div className="space-y-1">
-          <Label htmlFor="email">{labels.email}</Label>
-          <Input id="email" type="email" />
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="password">{labels.password}</Label>
-          <Input id="password" type="password" />
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="confirm">{labels.confirmPassword}</Label>
-          <Input id="confirm" type="password" />
-        </div>
+        <Form {...form}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{labels.email}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{labels.password}</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{labels.confirmPassword}</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </Form>
       </CardContent>
       <CardFooter className="flex justify-between gap-4">
-        <Button className="w-1/2" variant={"outline"} type="reset">{labels.reset}</Button>
-        <Button className="w-1/2" type="submit">{labels.getOTP}</Button>
+        <Button className="w-1/2" variant="outline" type="reset" onClick={form.reset}>
+          {labels.reset}
+        </Button>
+        <Button className="w-1/2" type="submit" onClick={form.handleSubmit(onSubmit)}>
+          {labels.getOTP}
+        </Button>
       </CardFooter>
     </Card>
   );
